0885d19dbeb6fab49dc2d2dfab20671c10e30b9c
jackthomsonn <hello@jackthomson.co.uk>
2019-04-19 10:47:10 +0100

    Remove Console Log

--- src/app.ts
+++ src/app.ts
@@ -50,7 +50,6 @@ export class Application {
   }
 
   private handleApplicationErrors() {
-    console.log('Handle');
     this.app.use((err: any, _req: any, res: any, _next: any) => {
       res.status(err.status).send({
         message: err.message,
